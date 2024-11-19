828e9a27e42593719b5ac597a876338654869179
John B <sopues@gmail.com>
2024-11-19 21:29:40 +0100

    work

--- views/dnaroutes/dnaroutes.js
+++ views/dnaroutes/dnaroutes.js
@@ -4,6 +4,15 @@ window.DNARoutesView = class DNARoutesView extends View {
     static people = new Map();
     static cc7MinPrivateId = 0;
 
+    other_person = "";
+    our_id = "";
+    cont_sel = "";
+
+    constructor() {
+        super();
+        console.log("CTOR");
+    }
+
     meta() {
         return {
             title: "DNA Routes",
@@ -13,6 +22,7 @@ window.DNARoutesView = class DNARoutesView extends View {
 
     static addPeople(profiles) {
         let nrAdded = 0;
+        let oldestBirth = 9999;
         for (const person of profiles) {
             //What does the + do?
             let id = +person.Id;
@@ -32,6 +42,10 @@ window.DNARoutesView = class DNARoutesView extends View {
                 
                 ++nrAdded;
 
+                let year = parseInt(person.BirthDateDecade.replace("s", ""), 10);
+                if(oldestBirth > year) {
+                    oldestBirth = year;
+                }
                 DNARoutesView.people.set(id, person);
             } else {
                 console.log(`Already added ${id}`);
@@ -42,42 +56,79 @@ window.DNARoutesView = class DNARoutesView extends View {
         let work = new Set();
         let added_work = 0;
         for(const [k, p] of DNARoutesView.people) {
-            console.log(`Checking ${p.Id} ${p.Parents}`);
+            //console.log(`Checking ${p.Id} ${p.Parents}`);
             if((p.Parents[0] != 0 && !DNARoutesView.people.has(p.Parents[0])) || (p.Parents[1] != 0 && !DNARoutesView.people.has(p.Parents[1]))) {
                 added_work++;
                 work.add(p.Id);
-                console.log(`Work: ${p.Name}`);
+                console.log(`Work: ${p}`);
             }
         }
+        const $ = document.querySelector.bind(document);
+
+        if ($('div#unfinished')) {
+            $('div#unfinished').innerText = added_work.toString();
+        } else {
+            console.log("No unfinished");
+        }
+        if ($('div#total-persons')) {
+            $('div#total-persons').innerText = DNARoutesView.people.size.toString();
+        }
+        if ($('div#oldest-birth')) {
+            $('div#oldest-birth').innerText = oldestBirth.toString();
+        }
         console.log(`New work left to do: ${work.size} and added ${added_work}`);
+
+        console.log(`Other is: ${this.other_person}`)
+        if(this.other_person) {
+            //Yeah, we can do shit!
+            console.log(this.cont_sel);
+            document.querySelector(cont_sel).innerText = `Ok, path between ${this.person_id} and ${this.other_person}`;
+
+        }
     }
 
     async init(container_selector, person_id) {
+        this.our_id = person_id;
+        this.cont_sel = container_selector;
         console.log(person_id);
         const personData = await WikiTreeAPI.getPerson(DNARoutesView.APP_ID, person_id, ["FirstName"]);
         console.log(personData);
         const name = personData["_data"]["FirstName"];
         document.querySelector(container_selector).innerText = `Hello, ${name}`;
 
-        if ($('#view-options').length) {
-            $('#view-options').remove();
+        if ($('#main-status').length) {
+            $('#main-status').remove();
         }
 
         const displayOptionsHTML = `
+        <div style="display:flex;flex-wrap:none" id="main-status">
         <div id="view-options">
-            <label for="display-mode">Other relative:</label>
-            <input type="text">
+            <label for="our-go">Other relative:</label>
+            <input type="text" id="other_person">
             <input type="submit" id="our-go" value="GO">
+        </div>
+        <div style="display:flex" id="summary-info">
+            Profiles total: <div id="total-persons">N/A</div>&nbsp;
+            Profiles not finished: <div id="unfinished">N/A</div>&nbsp;
+            Oldest birthdate: <div id="oldest-birth">N/A</div>&nbsp;
+        </div>
         </div>
         `;
 
         $('main').append(displayOptionsHTML);
 
-        // Handle view mode switching
-        $('#our-go').on('click', function () {
-            console.log("asdasdasd");
-        });
+        this.getProfiles(person_id);
 
+        $('#our-go').on('click', () => {
+            const $ = document.querySelector.bind(document);
+            console.log($('input#other_person').value);
+            this.getProfiles($('#other_person').value);
+            this.other_person = $('#other_person').value;
+            console.log(`Set other: ${this.other_person}`)
+        });
+    }
+
+    async getProfiles(person_id) {
         DNARoutesView.cancelLoadController = new AbortController();
 
         const starttime = performance.now();
@@ -86,7 +137,7 @@ window.DNARoutesView = class DNARoutesView extends View {
                 {
                     appId: DNARoutesView.APP_ID,
                     action: "getPeople",
-                    keys: "Bäckstrand-4",
+                    keys: person_id,
                     nuclear: 0,
                     minGeneration: 0,
                     ancestors: 10,
